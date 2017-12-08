d1df2a240550305f8c17786f06ed8b30c4f96934
Even Rouault <even.rouault@spatialys.com>
2017-12-08 18:39:30 +0100

    gdal/run_code_coverage_upload.sh: force push changes to reinit repo

--- gdal/run_code_coverage_upload.sh
+++ gdal/run_code_coverage_upload.sh
@@ -25,12 +25,12 @@ git config user.email "gdalbot@gdal.bot"
 git config user.name "GDAL coveragebot"
 git config push.default matching
 git remote add origin git@foo.github.com:rouault/gdalautotest-coverage-results.git
-git fetch
-git pull origin master
-rm -rf *
+#git fetch
+#git pull origin master
+#rm -rf *
 cp -r ../coverage_html .
 echo "Results of coverage of GDAL autotest" > README.md
 echo "See http://rawgithub.com/rouault/gdalautotest-coverage-results/master/coverage_html/index.html" >> README.md
 git add -A
 git commit -m "update with results of commit https://github.com/rouault/gdal_coverage/commit/$TRAVIS_COMMIT"
-git push origin master
+git push -f origin master
